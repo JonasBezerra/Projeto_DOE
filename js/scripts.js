ca15b3d1307017781db6be1f3e851a95c4bf227b
Jonas Bezerra da Silva <jonas.official2019@gmail.com>
2020-09-21 21:35:23 -0400

    commite finalização front-end

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var a = window.document.querySelector('header button ')
+a.addEventListener('click', sumirForm)
+
+function sumirForm(){
+    window.document.querySelector('.form').classList.toggle('hiden')
+         
+}
